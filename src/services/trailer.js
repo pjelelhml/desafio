5d041c94e1c856f866dab9725da8775cebcaf75c
pjelelhml <paulo3385@hotmail.com>
2021-01-26 16:18:00 -0400

    salvando

--- src/services/trailer.js
+++ src/services/trailer.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Trailer = titleTrailer => {
-  let youtubeAPI = 'AIzaSyAwGfCr1W-kK8D_7m40IjfYNtwDxXVKJUo'
+  let youtubeAPI = ''
   return axios.create({
     baseURL: `https://www.googleapis.com/youtube/v3/search?part=snippet&key=${youtubeAPI}q=${titleTrailer}&type=video&maxResults=1`
   });
