7511303feb32c11b8a7b4e630c496970ce3fc05c
pjelelhml <paulo3385@hotmail.com>
2021-01-26 16:01:18 -0400

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Movies = type => {
-  let movieAPI = ''
+  let movieAPI = '8d65cb0c'
   return axios.create({
     baseURL: `http://www.omdbapi.com/?apikey=${movieAPI}&${type}`
   });
